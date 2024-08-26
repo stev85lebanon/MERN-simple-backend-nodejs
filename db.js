a9e91ed3d07d954ea33d9ffdf291dc2d77b3d8d5
stev85lebanon <mostafaljamal@outlook.com>
2024-08-26 10:29:19 +0200

    update without comment

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
-// const mongoose = require('mongoose');
 const {Schema,connect,model} = require('mongoose');
 
 const CreateConnection=()=>{
     connect('mongodb://127.0.0.1:27017/users')
     .then(() => console.log('Connected!'))
-  //   .catch((e)=>console.error("failed connect!"+e));
 }
-// CreateConnection()
 
 const employeeSchema= new Schema({
     name:{type:String},
@@ -27,9 +24,7 @@ const createEmployee=async(_name,_age,_department,_isApproved)=>{
     })
     const result=await muhammad.save() 
     return result
-    // console.log(result)
 }
-// createEmployee("samer",37,"accounting",true)
 const getUser=async()=>{
     await CreateConnection()
     const employee=await Employee
@@ -39,19 +34,13 @@ const getUser=async()=>{
     // .select({name:1,age:1})//column that want to retrieve
     // .limit(2)//number that want to retrieve
     return employee
-    // console.log(employee)
 }
-// getUser()
-
 const getUserById=async(id)=>{
     await CreateConnection()
 
-    // const employee=await Employee.find({_id:id})
     const employee=await Employee.findById(id)
     return employee
-    // console.log(employee)
 }
-// getUserById("66be79e3627615636f800b66")
 const userUpdate=async(id)=>{
     const employee= await Employee.findByIdAndUpdate({_id:id},req.body,{new:true})
     if(!employee){
@@ -62,16 +51,12 @@ const userUpdate=async(id)=>{
     }
     console.log(result)
 }
-// userUpdate("66be79e3627615636f800b66")
 const userDelete=async(id)=>{
     await CreateConnection()
 
     const employee= await Employee.findByIdAndDelete(id)
     return employee
-    // if(employee)
-
-    // console.log(employee+"deleted")
+    
 }
-// userDelete("66be79e3627615636f800b66")
 
 module.exports={createEmployee,CreateConnection,getUser,getUserById,userDelete,Employee}
